c21672ecd4f7bad2c2a8ad34b8f54d8468d941ab
NotBrokenSemSogg <92069786+NotBrokenSemSogg@users.noreply.github.com>
2022-02-17 14:40:55 -0800

    Add files via upload

---
+++ random string.py
@@ -0,0 +1,23 @@
+from ntpath import join
+from os import system
+from posixpath import split
+import time
+import datetime
+import string
+import random
+
+randString = ''.join(random.choices(string.printable.replace(string.whitespace, "_"), k=random.randint(1, 128)))
+xTime = datetime.datetime.now()
+
+
+
+try: 
+    with open("output.txt", "x") as output_file:
+        print(xTime, "\n--------START--------\n" + randString, "\n--------END--------\n", file=output_file)
+        output_file.close()
+        print(xTime, "\n--------START--------\n" + randString, "\n--------END--------\n")
+except FileExistsError:
+    with open("output.txt", "a") as output_file:
+        print(xTime, "\n--------START--------\n" + randString, "\n--------END--------\n", file=output_file)
+        output_file.close()
+        print(xTime, "\n--------START--------\n" + randString, "\n--------END--------\n")
